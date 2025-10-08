4b423dede5da0a7c729dcdbec28f7ac68b97b28a
Pepe <josejesusvelazquez.24@campuscamara.es>
2025-10-08 09:16:15 +0200

    probando1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    //variable a la que le asigno mi nombre
+    $nombre="Jose Jesus Velazquez Perez";
+    //Linea que muestra por pantalla el correspondiente mensaje con el contenido de la variable nombre
+    echo "Hola, mi nombre es " $nombre;
+    //Constante con el valor de PI
+    define("PI", 3.1416);
+    //Variable Nombre con la n en mayúscula
