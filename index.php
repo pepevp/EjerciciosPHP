713591f469402c2f85762f3b4b51b5868e3cde8d
Pepe <josejesusvelazquez.24@campuscamara.es>
2025-10-08 09:29:26 +0200

    Ejercicio 1 completo

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     echo "Hola, mi nombre es ", $nombre;
     //Constante con el valor de PI
     define("PI", 3.1416);
-    //Linea que muestra por pantalla el valor de PI
+    //Linea que mete un salto de linea
     echo "<br></br>";
+    //Linea que muestra por pantalla el valor de PI
     echo PI;
+    //Linea que mete un salto de linea
     echo "<br></br>";
     //Variable Nombre con la n en mayúscula para comprobar que php diferencia entre mayúsculas y minúsculas
     $Nombre="Jesús Sanchez Catalán";
