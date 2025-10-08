90b6a3240051342a1fe6bfe973c8f46ab452a122
Pepe <josejesusvelazquez.24@campuscamara.es>
2025-10-08 09:21:01 +0200

    Ejercicio1

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
     //variable a la que le asigno mi nombre
     $nombre="Jose Jesus Velazquez Perez";
     //Linea que muestra por pantalla el correspondiente mensaje con el contenido de la variable nombre
-    echo "Hola, mi nombre es " $nombre;
+    echo "Hola, mi nombre es ", $nombre;
     //Constante con el valor de PI
     define("PI", 3.1416);
-    //Variable Nombre con la n en mayúscula
+?>
+    <br></br>
+<?php
+    //Variable Nombre con la n en mayúscula para comprobar que php diferencia entre mayúsculas y minúsculas
+    $Nombre="Jesús Sanchez Catalán";
+    echo "Hola, mi nombre es ", $Nombre;
+?>
